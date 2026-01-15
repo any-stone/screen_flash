90f9addefe9a3db1e78f30e2c5938b82600f18cc
Annie Kuxa <kuxa.business@gmail.com>
2026-01-14 21:36:26 -0400

    Initial commit from template

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Hello, Halifax! program
+//Annie Kuxa
+//January 3, 2026
+
+#include "SSDL.h"
+
+int main (int argc, char** argv)
+{
+    sout << "Hello, Halifax! (Press any key to quit.)\n";
+    SSDL_WaitKey (); //Waiting for user to hit any key
+    
+    return 0;
+}
